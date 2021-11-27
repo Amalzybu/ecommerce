2047db8d6f796eabcaea0b88a0f3b0643879859f
amalk_beacon <amalk@beaconinfo.tech>
2021-11-27 23:55:59 +0530

    json web token

--- core/customer/customer_app.js
+++ core/customer/customer_app.js
@@ -1,13 +1,18 @@
 const { sequelize, User,Orders,OrderItems,Products, Post } = require("../../models");
 const products = require("../../models/products");
 const bcrypt = require("bcrypt");
+const config = require("../../config/auth.config");
+var jwt = require("jsonwebtoken");
 
 module.exports = {
 
 
 allOrders:async (req, res) => {
     try {
-      const products = await User.findAll({
+      const products = await User.findOne({
+        where:{
+          id: req.userId
+        },
         include: 'orders',
       })
   
@@ -111,6 +116,48 @@ customerSignUp:async (req,res)=>{
     console.log(err)
     return res.status(500).json({ error: 'Something went wrong' })
   }
+},
+
+customerSignIn: async (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid Password!"
+        });
+      }
+
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400 // 24 hours
+      });
+
+     
+        res.status(200).send({
+          id: user.id,
+          email: user.email,
+          accessToken: token
+        });
+    
+    })
+    .catch(err => {
+      console.log("sssss"+err)
+      res.status(500).send({ message: err.message });
+    });
 }
+
 
 }
--- core/customer/customer_routes.js
+++ core/customer/customer_routes.js
@@ -1,14 +1,17 @@
 var express = require('express');
 customer=require("./customer_app");
 var router = express.Router();
+const authJwt = require("../middleware/authJwt");
 
 /* GET users listing. */
 
 
-router.get('/orders',customer.allOrders);
-router.post('/orders-details',customer.orderDetails);
-router.post('/cart-checkout',customer.checkoutCart);
+router.get('/orders',[authJwt.verifyToken],customer.allOrders);
+router.post('/orders-details',[authJwt.verifyToken],customer.orderDetails);
+router.post('/cart-checkout',[authJwt.verifyToken],customer.checkoutCart);
 router.post('/signup',customer.customerSignUp);
+router.post('/signin',customer.customerSignIn);
+
 
 
 
